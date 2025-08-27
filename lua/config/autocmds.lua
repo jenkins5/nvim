3fe04c63893f5861b1d2f5bb4e688b67794d438f
jenkins5 <demonddare@Demonds-MacBook-Pro.local>
2025-08-27 22:21:55 +0800

    feat(config): add initial configuration
    - add init.lua entrypoint
    - add autocmds.lua for basic events (highlight yank, mode change, etc.)
    - add keymaps.lua with initial key mappings
    - add options.lua for editor defaults
    - add core.lua for base setup
    - update .gitignore to exclude build artifacts

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,94 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- set no comment in next line
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	command = "set formatoptions-=ro",
+})
+
+-- highlight yank
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- fix select mode diagnostic
+local diag_in_select = augroup("diag_in_select", { clear = true })
+autocmd("ModeChanged", {
+	group = diag_in_select,
+	pattern = "*:s",
+	callback = function()
+		vim.diagnostic.enable(false)
+	end,
+})
+autocmd("ModeChanged", {
+	group = diag_in_select,
+	pattern = "[is]:n",
+	callback = function()
+		vim.diagnostic.enable()
+	end,
+})
+
+-- For settings that need to be reapplied on colorscheme changes
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = vim.api.nvim_create_augroup("refresh_transparency", { clear = true }),
+	callback = function()
+		vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+		vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
+		-- Reapply settings for floating windows on colorscheme change
+		vim.cmd("highlight NormalFloat guibg=NONE")
+		vim.cmd("highlight FloatBorder guibg=NONE")
+
+		vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TabLineNone", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
+	end,
+})
+
+-- fix padding on terminal
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if not normal.bg then
+			return
+		end
+		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+	end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+	callback = function()
+		io.write("\027]111\027\\")
+	end,
+})
+
+-- auto trigger fold
+local auto_view = augroup("auto_view", { clear = true })
+autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
+	desc = "Save view with mkview for real files",
+	group = auto_view,
+	callback = function(args)
+		if vim.b[args.buf].view_activated then
+			vim.cmd.mkview({ mods = { emsg_silent = true } })
+		end
+	end,
+})
+autocmd("BufWinEnter", {
+	desc = "Try to load file view if available and enable view saving for real files",
+	group = auto_view,
+	callback = function(args)
+		if not vim.b[args.buf].view_activated then
+			local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+			local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+			local ignore_filetypes = { "gitcommit", "gitrebase" }
+			if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+				vim.b[args.buf].view_activated = true
+				vim.cmd.loadview({ mods = { emsg_silent = true } })
+			end
+		end
+	end,
+})
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,94 @@
+local mode_n = { "n" }
+local mode_i = { "i" }
+local mode_v = { "v" }
+local mode_nv = { "n", "v" }
+
+local keymap = {
+    { mode = mode_n,      from = "S",               to = "<cmd>w<CR>",                                              desc = "save file" },
+    { mode = mode_n,      from = "Q",               to = "<cmd>q<CR>",                                              desc = "quit" },
+    { mode = mode_nv,     from = ";",               to = ":",                                                       desc = "command-line mode" },
+    { mode = mode_v,      from = "Y",               to = "\"+y",                                                    desc = "yank to sytem clipboard" },
+    { mode = mode_nv,     from = "`",               to = "~",                                                       desc = "toggle case" },
+
+		-- Movement
+    { mode = mode_nv,     from = "u",               to = "k",                                                       desc = "move up" },
+    { mode = mode_nv,     from = "e",               to = "j",                                                       desc = "move down" },
+    { mode = mode_nv,     from = "n",               to = "h",                                                       desc = "move left" },
+    { mode = mode_nv,     from = "i",               to = "l",                                                       desc = "move right" },
+    { mode = mode_nv,     from = "U",               to = "5k",                                                      desc = "move up 5 lines" },
+    { mode = mode_nv,     from = "E",               to = "5j",                                                      desc = "move down 5 lines" },
+    { mode = mode_nv,     from = "N",               to = "0",                                                       desc = "move to line start" },
+    { mode = mode_nv,     from = "I",               to = "$",                                                       desc = "move to line end" },
+    { mode = mode_nv,     from = "gu",              to = "gk",                                                      desc = "move up (wrap-aware)" },
+    { mode = mode_nv,     from = "ge",              to = "gj",                                                      desc = "move down (wrap-aware)" },
+    { mode = mode_nv,     from = "h",               to = "e",                                                       desc = "jump to end of word" },
+    { mode = mode_nv,     from = "<C-U>",           to = "5<C-y>",                                                  desc = "scroll up 5 lines" },
+    { mode = mode_nv,     from = "<C-E>",           to = "5<C-e>" ,                                                 desc = "scroll down 5 lines" },
+    { mode = mode_n,      from = "ci",              to = "cl",                                                      desc = "change one char" },
+    { mode = mode_n,      from = "cn",              to = "ch",                                                      desc = "change left char" },
+    { mode = mode_n,      from = "ck",              to = "ci",                                                      desc = "change inside object" },
+    { mode = mode_n,      from = "c,.",             to = "c%",                                                      desc = "change inside brackets" },
+    { mode = mode_n,      from = "yh",              to = "ye",                                                      desc = "yank to work end" },
+
+		-- Actions
+    { mode = mode_n,      from = "l",               to = "u",                                                       desc = "undo" },
+    { mode = mode_nv,     from = "k",               to = "i",                                                       desc = "insert before cursor" },
+    { mode = mode_nv,     from = "K",               to = "I",                                                       desc = "insert at line start" },
+
+		-- Search
+    { mode = mode_n,      from = "=",               to = "nzz",                                                     desc = "next search (centered)" },
+    { mode = mode_n,      from = "-",               to = "Nzz",                                                     desc = "prev search (centered)" },
+    { mode = mode_n,      from = "<leader><CR>",    to = "<cmd>nohlsearch<CR>",                                     desc = "clear search highlight" },
+
+		-- Useful actions
+    { mode = mode_nv,     from = ",.",              to = "%",                                                       desc = "jump to matching bracket" },
+    { mode = mode_i,      from = "<c-y>",           to = "<ESC>A {}<ESC>i<CR><ESC>ko",                              desc = "insert braces block" },
+    { mode = mode_n,      from = "\\v",             to = "v$h",                                                     desc = "visual select to line end -1" },
+    { mode = mode_i,      from = "<c-a>",           to = "<ESC>A",                                                  desc = "append at line end" },
+
+		-- Window & splits
+    { mode = mode_n,      from = "<leader>w",       to = "<C-w>w",                                                  desc = "switch window" },
+    { mode = mode_n,      from = "<leader>u",       to = "<C-w>k",                                                  desc = "move to window up" },
+    { mode = mode_n,      from = "<leader>e",       to = "<C-w>j",                                                  desc = "move to window down" },
+    { mode = mode_n,      from = "<leader>n",       to = "<C-w>h",                                                  desc = "move to window left" },
+    { mode = mode_n,      from = "<leader>i",       to = "<C-w>l",                                                  desc = "move to window right" },
+    { mode = mode_n,      from = "qf",              to = "<C-w>o",                                                  desc = "keep only current window" },
+    { mode = mode_n,      from = "s",               to = "<nop>",                                                   desc = "disable default s" },
+    { mode = mode_n,      from = "su",              to = "<cmd>set nosplitbelow<CR>:split<CR>:set splitbelow<CR>",  desc = "split above" },
+    { mode = mode_n,      from = "se",              to = "<cmd>set splitbelow<CR>:split<CR>",                       desc = "split below" },
+    { mode = mode_n,      from = "sn",              to = "<cmd>set nosplitright<CR>:vsplit<CR>:set splitright<CR>", desc = "split left" },
+    { mode = mode_n,      from = "si",              to = "<cmd>set splitright<CR>:vsplit<CR>",                      desc = "split right" },
+    { mode = mode_n,      from = "<up>",            to = "<cmd>res +5<CR>",                                         desc = "increase window height" },
+    { mode = mode_n,      from = "<down>",          to = "<cmd>res -5<CR>",                                         desc = "decrease window height" },
+    { mode = mode_n,      from = "<left>",          to = "<cmd>vertical resize -5<CR>",                             desc = "decraase window left" },
+    { mode = mode_n,      from = "<right>",         to = "<cmd>vertical resize +5<CR>",                             desc = "increase window right" },
+    { mode = mode_n,      from = "srh",             to = "<C-w>b<C-w>K",                                            desc = "move window to top (horizontal)" },
+    { mode = mode_n,      from = "srv",             to = "<C-w>b<C-w>H",                                            desc = "move window to left (vertical)" },
+
+		-- Tab management
+    { mode = mode_n,      from = "tu",              to = "<cmd>tabe<CR>",                                           desc = "new tab" },
+    { mode = mode_n,      from = "tU",              to = "<cmd>tab split<CR>",                                      desc = "split current window to new tab" },
+    { mode = mode_n,      from = "tn",              to = "<cmd>-tabnext<CR>",                                       desc = "previous tab" },
+    { mode = mode_n,      from = "ti",              to = "<cmd>+tabnext<CR>",                                       desc = "next tab" },
+    { mode = mode_n,      from = "tmn",             to = "<cmd>-tabmove<CR>",                                       desc = "move tab left" },
+    { mode = mode_n,      from = "tmi",             to = "<cmd>+tabmove<CR>",                                       desc = "move tab right" },
+
+
+		-- Other
+    { mode = mode_n,      from = "<leader>sw",      to = "<cmd>set wrap!<CR>",                                      desc = "toggle warp" },
+    { mode = mode_n,      from = "<leader>sc",      to = "<cmd>set spell!<CR>",                                     desc = "toggle spell check" },
+    { mode = mode_n,      from = "<leader>ss",      to = ":%s/\\<<C-r><C-w>\\>//g<left><left>",                     desc = "global replacement", noise = true },
+    { mode = mode_n,      from = ",v",              to = "v%",                                                      desc = "select inside matching brackets" },
+    { mode = mode_n,      from = "<leader><esc>",   to = "<nop>",                                                   desc = "disable accidental <leader><esc>" },
+}
+
+for _, mapping in ipairs(keymap) do
+    if mapping.desc then
+        vim.keymap.set(mapping.mode, mapping.from, mapping.to, { noremap = true, silent = true, desc = mapping.desc })
+    else
+        vim.keymap.set(mapping.mode, mapping.from, mapping.to, { noremap = true, silent = true })
+    end
+    if mapping.noise == true then
+        vim.keymap.set(mapping.mode, mapping.from, mapping.to, { noremap = true })
+    end
+end
